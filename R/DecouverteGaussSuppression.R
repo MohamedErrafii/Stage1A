ff29794b7fc13d7535eb14ad6460402651347fb7
mohamed <mohamed.errafii@insee.fr>
2024-06-21 10:07:34 +0000

    12:07/21/06/2024

--- R/DecouverteGaussSuppression.R
+++ R/DecouverteGaussSuppression.R
@@ -1,3 +1,9 @@
 rm(list=ls())
 install.packages("GaussSuppression")
-require("GaussSuppresion")
+library(GaussSuppression)
+z1 <- SSBtoolsData("z1")
+z2 <- SSBtoolsData("z2")
+z3 <- SSBtoolsData("z3")
+# Ordinary suppressions
+a <- GaussSuppressionFromData(z1, 1:2, 3, maxN = 5)
+b <- GaussSuppressionFromData(z2, 1:4, 5, maxN = 1)
